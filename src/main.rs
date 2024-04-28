a70fc9773047e76dbd380d8db633881145f711e8
GitManTechnologies <119501407+SuiNagahoshi@users.noreply.github.com>
2024-04-28 10:27:16 +0900

    add read_file: success

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        panic!("ERROR: Invalid args.");
+    }
+
+    let file = BufReader::new(File::open(args.get(1).unwrap()).expect("ERROR: File not found."));
+    let operations = file
+        .lines()
+        .map(|line| line.unwrap())
+        .collect::<Vec<String>>();
+    for i in &operations {
+        println!("{}", i);
+    }
 }
