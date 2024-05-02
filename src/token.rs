589fad5d3581eddb68dca97062b59343289dff35
GitManTechnologies <119501407+SuiNagahoshi@users.noreply.github.com>
2024-05-02 12:04:42 +0900

    add parser: done

---
+++ src/token.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+
+pub enum Register {
+    A,
+    B,
+}
+
+impl From<String> for Register {
+    fn from(a: String) -> Self {
+        if a == *"A" {
+            Register::A
+        } else if a == *"B" {
+            Register::B
+        } else {
+            panic!("Parse failed")
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Token {
+    Mov(Register, u8),
+    MovAB,
+    MovBA,
+    Add(Register, u8),
+    Jmp(u8),
+    Jnc(u8),
+    In(Register),
+    OutIm(u8),
+    OutB,
+}
