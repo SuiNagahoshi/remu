4a76f646bda72c8bbb3d7c713292a1b1f0aa7394
GitManTechnologies <119501407+SuiNagahoshi@users.noreply.github.com>
2024-05-09 17:50:16 +0900

    call td4 emulator with clap : success

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,38 @@ fn main() {
             //    println!("{}", i);
             //}
             println!("{:?}", operations);
+            let mut parser = parser::Parser::new(operations);
+            let tokens = match parser.parse() {
+                Ok(tokens) => tokens,
+                Err(err) => panic!("{:?}", err),
+            };
+
+            println!("{:?}", tokens);
+
+            let compiler = Compiler::new();
+            let program = match compiler.compile(tokens) {
+                Ok(program) => program,
+                Err(err) => panic!("{:?}", err),
+            };
+
+            println!("{:?}", program);
+
+            /*
+            let operations = file
+                .chars()
+                .into_iter()
+                .map(|char| char.to_digit(10000).unwrap() as u8)
+                .collect();
+            */
+
+            let rom = Rom::new(program);
+            let register = Register::new();
+            let port = Port::new(0b0000, 0b0000);
+            let mut emulator = CpuEmulator::with(register, port, rom);
+            match emulator.exec() {
+                Ok(_) => (),
+                Err(error) => panic!("{:?}", error),
+            }
         }
     }
 }
